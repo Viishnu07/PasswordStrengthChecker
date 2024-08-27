0c5c2d392cb0822b87a6848562a7384c71d54523
Dell Latitude 7300 <Dell Latitude 7300@VISH15>
2024-08-27 09:18:01 +0800

    Add project files.

---
+++ Week2_ISC/Form1.Designer.vb
@@ -0,0 +1,83 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtPwd = New System.Windows.Forms.TextBox()
+        Me.lblPassword = New System.Windows.Forms.Label()
+        Me.lblProgressbar = New System.Windows.Forms.Label()
+        Me.pbStrength = New System.Windows.Forms.ProgressBar()
+        Me.SuspendLayout()
+        '
+        'txtPwd
+        '
+        Me.txtPwd.Location = New System.Drawing.Point(255, 132)
+        Me.txtPwd.Name = "txtPwd"
+        Me.txtPwd.Size = New System.Drawing.Size(250, 22)
+        Me.txtPwd.TabIndex = 0
+        '
+        'lblPassword
+        '
+        Me.lblPassword.AutoSize = True
+        Me.lblPassword.Location = New System.Drawing.Point(147, 138)
+        Me.lblPassword.Name = "lblPassword"
+        Me.lblPassword.Size = New System.Drawing.Size(70, 16)
+        Me.lblPassword.TabIndex = 1
+        Me.lblPassword.Text = "Password:"
+        '
+        'lblProgressbar
+        '
+        Me.lblProgressbar.AutoSize = True
+        Me.lblProgressbar.Location = New System.Drawing.Point(147, 342)
+        Me.lblProgressbar.Name = "lblProgressbar"
+        Me.lblProgressbar.Size = New System.Drawing.Size(119, 16)
+        Me.lblProgressbar.TabIndex = 2
+        Me.lblProgressbar.Text = "Password Strength"
+        '
+        'pbStrength
+        '
+        Me.pbStrength.Location = New System.Drawing.Point(150, 245)
+        Me.pbStrength.Name = "pbStrength"
+        Me.pbStrength.Size = New System.Drawing.Size(458, 54)
+        Me.pbStrength.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.pbStrength)
+        Me.Controls.Add(Me.lblProgressbar)
+        Me.Controls.Add(Me.lblPassword)
+        Me.Controls.Add(Me.txtPwd)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtPwd As TextBox
+    Friend WithEvents lblPassword As Label
+    Friend WithEvents lblProgressbar As Label
+    Friend WithEvents pbStrength As ProgressBar
+End Class
---
+++ Week2_ISC/Form1.vb
@@ -0,0 +1,55 @@
+﻿Public Class Form1
+    Private StrengthWords() As String = {"Invalid", "Very Weak", "Weak", "Better", "Medium", "Strong", "Perfect"}
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles txtPwd.TextChanged
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+    Sub CalculateMeter()
+        Dim score As Integer
+        Dim password As String = txtPwd.Text
+        If (password.Length > 6) Then score += 1 'Length more than 6 
+        If System.Text.RegularExpressions.Regex.IsMatch(password, "[a-z]") And
+                System.Text.RegularExpressions.Regex.IsMatch(password, "[A-Z]") Then
+            score += 1 'upper and lower case 
+        End If
+        If System.Text.RegularExpressions.Regex.IsMatch(password, "\d+") Then
+            score += 1 'number
+        End If
+        If System.Text.RegularExpressions.Regex.IsMatch(password, "[,!,@,#,$,%,^,&,*,?,_,~,-,/,"",]") Then
+            score += 1 'special character
+        End If
+        If (password.Length >= 10) Then score += 1 'length more than 9
+        If (password.Length > 15) Then score += 1 'length more than 15
+        pbStrength.Value = score / 6 * 100 'finding percentage to increase 
+        lblProgressbar.Width = 50 * score 'label width is not auto so setting it to show color amount
+        lblProgressbar.Text = StrengthWords(score) 'Getting strength word from string array declared above 
+        lblProgressbar.TextAlign = ContentAlignment.MiddleCenter 'aligning to center can be done one time in design
+        lblProgressbar.BackColor = GetColor(score) 'Getting color and setting 
+        pbStrength.ForeColor = GetColor(score) 'does not work unless you disable Visual Styles from application properties
+    End Sub
+    Private Function GetColor(ByVal score As Integer) As Color
+        Select Case score
+            Case 0
+                Return Color.Red
+            Case 1
+                Return Color.Red
+            Case 2
+                Return Color.Purple
+            Case 3
+                Return Color.LightGreen
+            Case 4
+                Return Color.MediumSeaGreen
+            Case 5
+                Return Color.Green
+            Case 6
+                Return Color.DarkGreen
+        End Select
+    End Function
+    Private Sub txtPwd_KeyUp(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles txtPwd.KeyUp
+        CalculateMeter()
+    End Sub
+End Class
+
